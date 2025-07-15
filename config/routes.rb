3ee00227c22eb23ae77a6647efbc249eff540ebf
esteres <restreporamirezesteban4@gmail.com>
2025-07-15 07:12:53 +0000

    add versioned API routes for opportunities and apply endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :opportunities, only: [ :index, :create ] do
+        post :apply, on: :member
+      end
+    end
+  end
 end
